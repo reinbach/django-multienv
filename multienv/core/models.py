e95cb56e80e7e45aef6f78903777d9dce03fd871
Greg Reinbach <greg@reinbach.com>
2012-05-03 20:44:00 -0400

    environment model made abstract

--- multienv/core/models.py
+++ multienv/core/models.py
@@ -2,21 +2,29 @@ from django.db import models
 
 import utils
 
+#===============================================================================
 class EnvironmentManager(models.Manager):
     ENV = utils.get_default_environment()
-    
+
+    #---------------------------------------------------------------------------
     def get_query_set(self):
         return super(EnvirommentManager, self).get_query_set(
             using=utils.get_environment_db(self.ENV)
         )
 
+#===============================================================================
 class Environment(models.Model):
     objects = EnvironmentManager()
 
+    class Meta:
+        abstract = True
+
+    #---------------------------------------------------------------------------
     def save(self, request=None, *args, **kws):
         if request is not None:
             self.ENV = utils.get_environment(request)
         super(Environment, self).save(using=utils.get_environment_db(self.ENV))
 
+    #---------------------------------------------------------------------------
     def set_environment(self, env):
         self.ENV = env
