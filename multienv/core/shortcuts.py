f66a336708bcf2ad3d67297bfe34e6da0aad1530
Greg Reinbach <greg@reinbach.com>
2012-05-06 09:41:18 -0400

    Updated the home page text Now correctly handle next GET param when changing/setting environment

--- multienv/core/shortcuts.py
+++ multienv/core/shortcuts.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
@@ -6,6 +7,7 @@ from core import utils
 def request_to_response(request, template, context={}):
     # ensure we have the environment var set
     context.update(environment=utils.get_environment_data(request))
+    context.update(environments=settings.ENVIRONMENTS)
     return render_to_response(
         template,
         context,
--- multienv/core/views.py
+++ multienv/core/views.py
@@ -1,5 +1,4 @@
 from django import http
-from django.conf import settings
 from django.core.urlresolvers import reverse
 
 from core.shortcuts import request_to_response
@@ -7,14 +6,11 @@ from core import utils
 
 #---------------------------------------------------------------------------
 def home(request):
-    context = dict(
-        environments=settings.ENVIRONMENTS,
-    )
-    return request_to_response(request, 'index.html', context)
+    return request_to_response(request, 'index.html')
 
 #---------------------------------------------------------------------------
 def change_environment(request, env):
     utils.set_environment(request, env)
-    if next in request.GET:
+    if 'next' in request.GET:
         return http.HttpResponseRedirect(request.GET.get('next'))
     return http.HttpResponseRedirect(reverse('home'))
